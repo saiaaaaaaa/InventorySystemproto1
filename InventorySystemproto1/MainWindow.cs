6b02c565c27957427dd93d5779cbf1ddeaa0a7e9
SalazarLL <isaiahnoelsalazar474@gmail.com>
2024-11-20 20:00:10 +0800

    tab color change

--- InventorySystemproto1/MainWindow.cs
+++ InventorySystemproto1/MainWindow.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Forms;
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace InventorySystemproto1
 {
@@ -6,12 +8,16 @@ namespace InventorySystemproto1
     {
         Dashboard dashboard = new Dashboard();
         Inventory inventory = new Inventory();
+        List<Button> tabs = new List<Button>();
 
         public MainWindow(Startup startup)
         {
             InitializeComponent();
 
             startup.Hide();
+
+            tabs.Add(button1);
+            tabs.Add(button2);
         }
 
         private void MainWindow_FormClosing(object sender, FormClosingEventArgs e)
@@ -21,6 +27,8 @@ namespace InventorySystemproto1
 
         private void MainWindow_Load(object sender, System.EventArgs e)
         {
+            ChangeColor(0);
+
             dashboard.MdiParent = this;
             dashboard.Dock = DockStyle.Fill;
 
@@ -29,6 +37,8 @@ namespace InventorySystemproto1
 
         private void button1_Click(object sender, System.EventArgs e)
         {
+            ChangeColor(0);
+
             inventory.Hide();
             dashboard.MdiParent = this;
             dashboard.Dock = DockStyle.Fill;
@@ -38,11 +48,27 @@ namespace InventorySystemproto1
 
         private void button2_Click(object sender, System.EventArgs e)
         {
+            ChangeColor(1);
+
             dashboard.Hide();
             inventory.MdiParent = this;
             inventory.Dock = DockStyle.Fill;
 
             inventory.Show();
+        }
+
+        void ChangeColor(int activeForm)
+        {
+            tabs[activeForm].BackColor = Color.Green;
+            tabs[activeForm].ForeColor = Color.White;
+            foreach (Button b in tabs)
+            {
+                if (b != tabs[activeForm])
+                {
+                    b.BackColor = SystemColors.Control;
+                    b.ForeColor = SystemColors.ControlText;
+                }
+            }
         }
     }
 }
