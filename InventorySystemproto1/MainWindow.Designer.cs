550ffc21198126d3b671673d9e80533dec496398
SalazarLL <isaiahnoelsalazar474@gmail.com>
2024-11-20 21:01:57 +0800

    add other tabs

--- InventorySystemproto1/MainWindow.Designer.cs
+++ InventorySystemproto1/MainWindow.Designer.cs
@@ -30,9 +30,12 @@ namespace InventorySystemproto1
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -40,24 +43,56 @@ namespace InventorySystemproto1
             // 
             this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.button2);
             this.panel1.Controls.Add(this.button1);
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Margin = new System.Windows.Forms.Padding(0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 384);
+            this.panel1.Size = new System.Drawing.Size(200, 505);
             this.panel1.TabIndex = 4;
             // 
-            // button1
+            // button5
             // 
-            this.button1.Location = new System.Drawing.Point(3, 156);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(194, 50);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Dashboard (button muna)";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button5.Location = new System.Drawing.Point(3, 380);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(194, 50);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "About us (button muna)";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(3, 324);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(194, 50);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Order history (button muna)";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(3, 268);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(194, 50);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Sales (button muna)";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "User";
             // 
             // button2
             // 
@@ -69,20 +104,21 @@ namespace InventorySystemproto1
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // label1
+            // button1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(79, 67);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(29, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "User";
+            this.button1.Location = new System.Drawing.Point(3, 156);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(194, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Dashboard (button muna)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(705, 381);
+            this.ClientSize = new System.Drawing.Size(759, 502);
             this.Controls.Add(this.panel1);
             this.DoubleBuffered = true;
             this.IsMdiContainer = true;
@@ -103,6 +139,9 @@ namespace InventorySystemproto1
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- InventorySystemproto1/MainWindow.cs
+++ InventorySystemproto1/MainWindow.cs
@@ -6,8 +6,7 @@ namespace InventorySystemproto1
 {
     public partial class MainWindow : Form
     {
-        Dashboard dashboard = new Dashboard();
-        Inventory inventory = new Inventory();
+        List<Form> forms = new List<Form>();
         List<Button> tabs = new List<Button>();
 
         public MainWindow(Startup startup)
@@ -18,6 +17,15 @@ namespace InventorySystemproto1
 
             tabs.Add(button1);
             tabs.Add(button2);
+            tabs.Add(button3);
+            tabs.Add(button4);
+            tabs.Add(button5);
+
+            forms.Add(new Dashboard());
+            forms.Add(new Inventory());
+            forms.Add(new Sales());
+            forms.Add(new OrderHistory());
+            forms.Add(new AboutUs());
         }
 
         private void MainWindow_FormClosing(object sender, FormClosingEventArgs e)
@@ -27,37 +35,10 @@ namespace InventorySystemproto1
 
         private void MainWindow_Load(object sender, System.EventArgs e)
         {
-            ChangeColor(0);
-
-            dashboard.MdiParent = this;
-            dashboard.Dock = DockStyle.Fill;
-
-            dashboard.Show();
+            ChangeForm(0);
         }
 
-        private void button1_Click(object sender, System.EventArgs e)
-        {
-            ChangeColor(0);
-
-            inventory.Hide();
-            dashboard.MdiParent = this;
-            dashboard.Dock = DockStyle.Fill;
-
-            dashboard.Show();
-        }
-
-        private void button2_Click(object sender, System.EventArgs e)
-        {
-            ChangeColor(1);
-
-            dashboard.Hide();
-            inventory.MdiParent = this;
-            inventory.Dock = DockStyle.Fill;
-
-            inventory.Show();
-        }
-
-        void ChangeColor(int activeForm)
+        void ChangeForm(int activeForm)
         {
             tabs[activeForm].BackColor = Color.Green;
             tabs[activeForm].ForeColor = Color.White;
@@ -69,6 +50,47 @@ namespace InventorySystemproto1
                     b.ForeColor = SystemColors.ControlText;
                 }
             }
+
+            foreach (Form f in forms)
+            {
+                if (f != forms[activeForm])
+                {
+                    f.Hide();
+                }
+                else
+                {
+                    f.MdiParent = this;
+                    f.Dock = DockStyle.Fill;
+
+                    f.Show();
+                }
+
+            }
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            ChangeForm(0);
+        }
+
+        private void button2_Click(object sender, System.EventArgs e)
+        {
+            ChangeForm(1);
+        }
+
+        private void button3_Click(object sender, System.EventArgs e)
+        {
+            ChangeForm(2);
+        }
+
+        private void button4_Click(object sender, System.EventArgs e)
+        {
+            ChangeForm(3);
+        }
+
+        private void button5_Click(object sender, System.EventArgs e)
+        {
+            ChangeForm(4);
         }
     }
 }
